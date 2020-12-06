df8d91ecdd8b289ef415ee414d11ce22fdae0b78
Cho Sangho <97chos@naver.com>
2020-12-07 02:10:10 +0900

    Add Data Access Object pattern

--- HRWithFMDB/HRWithFMDB/DepartmentDAO.swift
+++ HRWithFMDB/HRWithFMDB/DepartmentDAO.swift
@@ -31,4 +31,96 @@ class DepartmentDAO {
         return db
 
     }()
+
+    init() {
+        self.fmdb.open()
+    }
+    deinit {
+        self.fmdb.close()
+    }
+
+    // 가져온 DB에게 데이터 쿼리를 요청하여 결과 집합을 반환받는 메소드
+    func find() -> [DepartRecord] {
+        // 반환할 데이터를 담을 [DepartRecord] 타입의 객체 정의
+        var departList = [DepartRecord]()
+
+        do {
+            // 1. 부서 정보 목록을 가져올 SQL 작성 및 쿼리 실행
+            let sql = """
+                SELECT depart_cd, depart_title, depart_addr FROM department
+                ORDER BY depart_cd ASC
+            """
+
+            let rs = try self.fmdb.executeQuery(sql, values: nil)
+
+            // 2. 결과 집합 추출
+            while rs.next() {
+                let departCd = rs.int(forColumn: "depart_cd")
+                let departTitle = rs.string(forColumn: "depart_title")
+                let departAddr = rs.string(forColumn: "depart_addr")
+
+                departList.append((Int(departCd),departTitle!,departAddr!))
+            }
+        } catch let error as NSError {
+            print("Failed: \(error.localizedDescription)")
+        }
+        return departList
+    }
+
+    // departCd를 이용하여 단일 테이블 지정 반환
+    func get(departCd: Int) -> DepartRecord? {
+        // 1. 질의 실행
+        let sql = """
+            SELECT depart_cd, depart_title, depart_addr FROM department
+            WHERE depart_cd = ?
+        """
+
+        // executeQuery(_:withArgumentsIn:) = 옵셔널 타입 반환
+        let rs = self.fmdb.executeQuery(sql, withArgumentsIn: [departCd])
+
+        // 2. 결과 집합 처리
+        if let _rs = rs {           // 결과 집합이 옵셔널 타입으로 반환되므로 이를 상수에 대입하여 옵셔널 해제
+            //MARK: - .next() 구문 동작 확인
+            _rs.next()
+
+            let departId = _rs.int(forColumn: "depart_cd")
+            let departTitle = _rs.string(forColumn: "depart_title")
+            let departAddr = _rs.string(forColumn: "depart_addr")
+
+            return ((Int(departId),departTitle!,departAddr!))
+        } else {
+            return nil
+        }
+    }
+
+    // 부서 정보 생성 메소드
+    func create(title: String, addr: String) -> Bool {
+        guard title != "" && addr != "" else {
+            return false
+        }
+
+        do {
+            let sql = """
+                INSERT INTO department (depart_title, depart_addr)
+                VALUES (?,?)
+            """
+            try self.fmdb.executeUpdate(sql, values: [title,addr])
+            return true
+        } catch let error as NSError {
+            print("Insert Error : \(error.localizedDescription)")
+            return false
+        }
+    }
+
+    // 부서 정보 삭제 메소드
+    func remove(departCd: Int) -> Bool {
+        do {
+            let sql = "DELETE FROM department WHERE depart_cd = ?"
+            try self.fmdb.executeUpdate(sql, values: [departCd])
+            return true
+        } catch let error as NSError {
+            print("DELETE Error = \(error.localizedDescription)")
+            return false
+        }
+    }
 }
--- HRWithFMDB/HRWithFMDB/DepartmentListVC.swift
+++ HRWithFMDB/HRWithFMDB/DepartmentListVC.swift
@@ -9,5 +9,5 @@ import Foundation
 import UIKit
 
 class DepartmentListVC: UITableViewController {
-    <#code#>
+
 }
