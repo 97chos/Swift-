e256e82719e9fdb1c2f341e7ce56249b89db3a05
Cho Sangho <97chos@naver.com>
2020-12-13 19:42:39 +0900

    How to use CoreData (Add log page)

--- Using CoreData/Using CoreData/ListVC.swift
+++ Using CoreData/Using CoreData/ListVC.swift
@@ -60,6 +60,15 @@ class ListVC: UITableViewController {
         object.setValue(contents, forKey: "contents")
         object.setValue(Date(), forKey: "regdate")
 
+        // Log 관리 객체 생성 및 어트리뷰트에 값 대입
+        let logObject = NSEntityDescription.insertNewObject(forEntityName: "Log", into: context) as! LogMO
+        
+        logObject.regdate = Date()
+        logObject.type = LogType.create.rawValue
+
+        // 게시글 객체의 logs 속성에 새로 생성된 로그 객체 추가
+        (object as! BoardMO).addToLogs(logObject)
+
         // 4. 영구 저장소에 커밋되고 나면 list 프로퍼티에 추가
         do {
             try context.save()
@@ -147,6 +156,15 @@ class ListVC: UITableViewController {
         object.setValue(contents, forKey: "contents")
         object.setValue(Date(), forKey: "regdate")
 
+        // Log 관리 객체 생성 및 어트리뷰트에 값 대입
+        let logObject = NSEntityDescription.insertNewObject(forEntityName: "Log", into: context) as! LogMO
+
+        logObject.regdate = Date()
+        logObject.type = LogType.edit.rawValue
+
+        // 게시글 객체의 logs 속성에 새로 생성된 로그 객체 추가
+        (object as! BoardMO).addToLogs(logObject)
+        
         do {
             try context.save()
             self.list = self.fetch()
@@ -216,8 +234,19 @@ class ListVC: UITableViewController {
 
         cell.textLabel?.text = title
         cell.detailTextLabel?.text = contents
+        cell.accessoryType = .detailButton
 
         return cell
+    }
+
+    // 액세서리 버튼 탭 메소드
+    override func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        let object = self.list[indexPath.row]
+        let uvc = self.storyboard?.instantiateViewController(withIdentifier: "LogVC") as! LogVC
+
+        uvc.board = (object as! BoardMO)
+
+        self.show(uvc, sender: self)
     }
 
 }
