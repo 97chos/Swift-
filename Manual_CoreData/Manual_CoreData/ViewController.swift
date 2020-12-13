e454360da6872574bee79b3c0b5c123fe43d81a5
Cho Sangho <97chos@naver.com>
2020-12-14 02:31:51 +0900

    How to add manual CoreData

---
+++ Manual_CoreData/Manual_CoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manual_CoreData
+//
+//  Created by sangho Cho on 2020/12/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
